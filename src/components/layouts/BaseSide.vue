b7898c3cb18142d17a43eb76e0926a3856727224
bboyhwi <bboyhwi@qq.com>
2023-03-31 15:34:35 +0800

    0.0.1

--- src/components/layouts/BaseSide.vue
+++ src/components/layouts/BaseSide.vue
@@ -1,10 +1,14 @@
 <template>
+  <el-radio-group v-model="isCollapse" style="margin-bottom: 20px">
+    <el-radio-button :label="false">expand</el-radio-button>
+    <el-radio-button :label="true">collapse</el-radio-button>
+  </el-radio-group>
   <el-menu
-    default-active="2"
-    class="el-menu-vertical-demo"
-    :collapse="isCollapse"
-    @open="handleOpen"
-    @close="handleClose"
+      default-active="2"
+      class="el-menu-vertical-demo"
+      :collapse="isCollapse"
+      @open="handleOpen"
+      @close="handleClose"
   >
     <el-sub-menu index="1">
       <template #title>
@@ -42,9 +46,9 @@
 <script lang="ts" setup>
 import { ref } from 'vue'
 import {
-  Location,
   Document,
   Menu as IconMenu,
+  Location,
   Setting,
 } from '@element-plus/icons-vue'
 
@@ -57,3 +61,9 @@ const handleClose = (key: string, keyPath: string[]) => {
 }
 </script>
 
+<style>
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+  width: 200px;
+  min-height: 400px;
+}
+</style>
