b6eb1fb10a93f9cba80d75589b07edf9f6b68117
bboyhwi <331883059@qq.com>
2023-03-31 15:34:35 +0800

    0.0.1

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <template>
-  <el-config-provider namespace="ep">
-    <BaseHeader />
-    <div style="display: flex">
-      <BaseSide />
-      <div>
-        <img alt="Vue logo" class="element-plus-logo" src="./assets/logo.png" />
-        <HelloWorld msg="Hello Vue 3.0 + Element Plus + Vite" />
-      </div>
-    </div>
-  </el-config-provider>
+  <div class="common-layout">
+    <el-container>
+      <el-aside >
+        <BaseSide />
+      </el-aside>
+      <el-container>
+        <el-header>
+          <BaseHeader />
+        </el-header>
+        <el-main>
+          <div>
+            <img alt="Vue logo" class="element-plus-logo" src="./assets/logo.png" />
+            <HelloWorld msg="Hello Vue 3.0 + Element Plus + Vite" />
+          </div>
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
 </template>
 
 <style>
